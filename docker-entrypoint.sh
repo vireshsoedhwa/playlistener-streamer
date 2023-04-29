b16d2f5792b5cd7512d7f4ee35dfb800e28232dd
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2023-04-29 05:32:42 -0700

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 set -e
->&2 echo "Make Database migrations"
-python manage.py makemigrations app
-echo "-------------------------------------------------------------------------------------------\n"
+# >&2 echo "Make Database migrations"
+# python manage.py makemigrations app
+# echo "-------------------------------------------------------------------------------------------\n"
 
->&2 echo "Run Database migrations"
-python manage.py migrate
-echo "-------------------------------------------------------------------------------------------\n"
+# >&2 echo "Run Database migrations"
+# python manage.py migrate
+# echo "-------------------------------------------------------------------------------------------\n"
 
 # Collect static files
->&2 echo "Collect static"
-python manage.py collectstatic --noinput
+# >&2 echo "Collect static"
+# python manage.py collectstatic --noinput
 
 # mkdir -p /code/data
-tail -f /dev/null
+# tail -f /dev/null
+touch /code/data/tag_cache
+chmod 777 /code/data/tag_cache
 
 >&2 echo "starting icecast..."
 /etc/init.d/icecast2 start
@@ -25,12 +27,12 @@ mpc ls | mpc add
 # mpc --stderr --no-daemon --verbose play
 mpc play
 
-# tail -f /dev/null
+tail -f /dev/null
 
->&2 echo "Starting Nginx..."
-nginx
+# >&2 echo "Starting Nginx..."
+# nginx
 
-celery -A tasks worker --loglevel=INFO
+# celery -A tasks worker --loglevel=INFO
 # nginx -g daemon off;
 # /usr/local/nginx/sbin/nginx -t
 exec "$@"
