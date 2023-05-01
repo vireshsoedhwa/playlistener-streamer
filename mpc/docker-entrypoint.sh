065ca5865f2980a95c322532f98bbd94fda03cba
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2023-05-01 05:24:11 -0700

    parameterize env

--- mpc/docker-entrypoint.sh
+++ mpc/docker-entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 set -e
 
-echo "starting running mpc ..."
-# sleep 10
-mpc --host=mpd_dancehall --port=6600 add /
-echo "playlist created..."
-mpc --host=mpd_dancehall --port=6600 random on
-mpc --host=mpd_dancehall --port=6600 repeat on
+echo "waiting for mpd to startup"
+sleep 20
+echo "starting running mpc default..."
+mpc --host=$MPD_HOST_DEFAULT --port=6600 add /
+mpc --host=$MPD_HOST_DEFAULT --port=6600 random on
+mpc --host=$MPD_HOST_DEFAULT --port=6600 repeat on
+mpc --host=$MPD_HOST_DEFAULT --port=6600 play
+
+echo "starting running mpc dancehall..."
+mpc --host=$MPD_HOST_DANCEHALL --port=6600 add /
+mpc --host=$MPD_HOST_DANCEHALL --port=6600 random on
+mpc --host=$MPD_HOST_DANCEHALL --port=6600 repeat on
+mpc --host=$MPD_HOST_DANCEHALL --port=6600 play
+
 # echo "current playlist content..."
 # mpc playlist
-mpc --host=mpd_dancehall --port=6600 play
-
 
 echo "Image version: `cat /code/version`"
 exec "$@"
