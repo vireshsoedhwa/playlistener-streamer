5d470199c645cbc9b0e3bc58f88d3560927008b5
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2023-05-01 06:00:47 -0700

    troubleshoot mpc deployment

--- mpc/docker-entrypoint.sh
+++ mpc/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 set -e
 
 echo "waiting for mpd to startup"
-sleep 20
-echo "starting running mpc default..."
-mpc --host=$MPD_HOST_DEFAULT --port=6600 add /
-mpc --host=$MPD_HOST_DEFAULT --port=6600 random on
-mpc --host=$MPD_HOST_DEFAULT --port=6600 repeat on
-mpc --host=$MPD_HOST_DEFAULT --port=6600 play
+sleep 10
+# echo "starting running mpc default..."
+# mpc --host=$MPD_HOST_DEFAULT --port=6600 add /
+# mpc --host=$MPD_HOST_DEFAULT --port=6600 random on
+# mpc --host=$MPD_HOST_DEFAULT --port=6600 repeat on
+# mpc --host=$MPD_HOST_DEFAULT --port=6600 play
 
-echo "starting running mpc dancehall..."
-mpc --host=$MPD_HOST_DANCEHALL --port=6600 add /
-mpc --host=$MPD_HOST_DANCEHALL --port=6600 random on
-mpc --host=$MPD_HOST_DANCEHALL --port=6600 repeat on
-mpc --host=$MPD_HOST_DANCEHALL --port=6600 play
+# echo "starting running mpc dancehall..."
+# mpc --host=$MPD_HOST_DANCEHALL --port=6600 add /
+# mpc --host=$MPD_HOST_DANCEHALL --port=6600 random on
+# mpc --host=$MPD_HOST_DANCEHALL --port=6600 repeat on
+# mpc --host=$MPD_HOST_DANCEHALL --port=6600 play
 
 # echo "current playlist content..."
 # mpc playlist
