963e74e18f2933dc90a4aaaf83496f0803c85088
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2023-05-16 23:18:10 -0700

    clear playlist

--- mpc/docker-entrypoint.sh
+++ mpc/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "mpc start"
 echo MPD_HOST=$MPD_HOST >> .env
 echo MPD_PORT=$MPD_PORT >> .env
+mpc --host=$MPD_HOST --port=$MPD_PORT clear
 mpc --host=$MPD_HOST --port=$MPD_PORT add /
 mpc --host=$MPD_HOST --port=$MPD_PORT random on
 mpc --host=$MPD_HOST --port=$MPD_PORT repeat on
