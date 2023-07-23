a7968a8c563d59d7ccbcd2dd9c0be75341488e27
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2023-07-22 21:37:09 -0700

    fix repeat bug

--- mpc/docker-entrypoint.sh
+++ mpc/docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo MPD_PORT=$MPD_PORT >> .env
 mpc --host=$MPD_HOST --port=$MPD_PORT clear
 mpc --host=$MPD_HOST --port=$MPD_PORT add /
 mpc --host=$MPD_HOST --port=$MPD_PORT random on
-mpc --host=$MPD_HOST --port=$MPD_PORT repeat on
+mpc --host=$MPD_HOST --port=$MPD_PORT repeat off
 mpc --host=$MPD_HOST --port=$MPD_PORT play
 
 echo "Image version: `cat /code/version`"
